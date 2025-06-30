789bf15cc238e546c2a98768872f31f5bd503424
HugoDPSz <hps.4002@gmail.com>
2025-06-30 16:49:51 -0300

    Correção/Atalização das rotas de mensalidade

--- app/routes/mensalidade.py
+++ app/routes/mensalidade.py
@@ -1,8 +1,8 @@
-### Passando por correções
-
 from flask import Blueprint, request, jsonify
 from app.models.mensalidade import db, Mensalidade
-from datetime import datetime
+from datetime import date
+from app.models.transaction import Transaction
+from app.models.account import Account
 
 bp_mensalidade = Blueprint('mensalidade', __name__, url_prefix='/api/mensalidade')
 
@@ -16,6 +16,33 @@ def buscar_por_id(id: int):
     mensalidade = Mensalidade.query.get_or_404(id)
     return jsonify(mensalidade.to_dict())
 
-@bp_mensalidade.route('/', methods=['POST'])
-def upar_mensaldiade():
-    data = request.json
+@bp_mensalidade.route('/mensalidades/<int:mensalidade_id>/pagar', methods=['POST'])
+def pagar_mensalidade(mensalidade_id):
+    mensalidade = Mensalidade.query.get_or_404(mensalidade_id)
+
+    if mensalidade.pago:
+        return jsonify({'error': 'Esta mensalidade já foi paga.'}), 400
+
+    mensalidade.pago = True
+    mensalidade.data_pagamento = date.today()
+
+    conta_receita_mensalidade = Account.query.filter_by(id=4010).first()
+    if not conta_receita_mensalidade:
+        return jsonify({'error': 'Conta de receita para mensalidades não configurada.'}), 500
+
+    nova_transacao = Transaction(
+        description=f'Recebimento da mensalidade ({mensalidade.mes_referencia}) de {mensalidade.desbravador.nome}',
+        amount=mensalidade.valor,
+        type='credit',
+        transaction_date=mensalidade.data_pagamento,
+        account_id=conta_receita_mensalidade.id,
+        mensalidade_id=mensalidade.id
+    )
+
+    db.session.add(nova_transacao)
+    db.session.commit()
+
+    return jsonify({
+        'message': 'Pagamento de mensalidade registrado com sucesso no caixa!',
+        'mensalidade': mensalidade.to_dict()
+    })
